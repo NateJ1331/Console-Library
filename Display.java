20f98406723367d0bf03fe62fd200e739d78381b
Nate <wildnate@byui.edu>
2024-07-04 16:47:06 -0600

    Fleshed out the menu

--- Display.java
+++ Display.java
@@ -1,7 +1,18 @@
 public class Display {
     
-    public void Menu()
+    public void Menu(String[] books)
     {
+
+        System.out.println("Books: ");
+
+        for(int i = 0; i < books.length; i++) {
+
+            System.out.println(books[i]);
+        }
+
+        System.out.println();
+        System.out.println();
+
         System.out.println("   MENU   ");
         System.out.println("1.      4.");
         System.out.println("2.      5.");
--- main.java
+++ main.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class main {
@@ -11,17 +10,20 @@ public class main {
 
         while (exit < 1) {
             
-            display.Menu();
+            String[] Books = {
+                "The Clockwork Angel","Frankenstien","The Dare","Stalking Jack the Ripper",
+                "Crazy Rich Asians","The Haunting","The Twin","Daughter of the Moon Godess",
+                "Daugter of the Moon Godess","Kept Secrets","The Outsiders","Pride and Prejudice","Beowulf"};
+
+            display.Menu(Books);
 
             String userinput = input.nextLine();
 
             switch (userinput) {
-                case "6":
-                    exit = 1;
-                    break;
+                case "6" -> exit = 1;
             
-                default:
-                    break;
+                default -> {
+                }
             }
 
         }
