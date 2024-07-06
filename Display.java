83f81380a3d8eab1d5e36a0be3b1ff6b0f97b141
Nate <wildnate@byui.edu>
2024-07-05 21:51:15 -0600

    Fleshed out the Menu options

--- Display.java
+++ Display.java
@@ -1,24 +1,29 @@
+import java.util.List;
+
 public class Display {
     
-    public void Menu(String[] books)
+    public void Menu()
     {
-
-        System.out.println("Books: ");
-
-        for(int i = 0; i < books.length; i++) {
-
-            System.out.println(books[i]);
-        }
-
-        System.out.println();
-        System.out.println();
-
         System.out.println("   MENU   ");
-        System.out.println("1.      4.");
-        System.out.println("2.      5.");
-        System.out.println("3.      6.");
+        System.out.println("1. Add Book        4. Display Books");
+        System.out.println("2. Remove Book     5. Check Out Book");
+        System.out.println("3. Search Books    6. Return Book");
+        System.out.println("7. Exit");
         System.out.println();
         System.out.print("> ");
     }
+
+
+    public void Books(List<String> books)
+    {   
+        System.out.println();
+        System.out.println("Books: ");
+
+        for (String book : books) {
+            System.out.println(book);
+        }
+        System.out.println();
+    }
+
      
 }
--- main.java
+++ main.java
@@ -1,26 +1,92 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class main {
     public static void main(String[] args)
     {
         Display display = new Display();
+        String[] ArrayBooks = {
+        "The Clockwork Angel","Frankenstien","The Dare","Stalking Jack the Ripper",
+        "Crazy Rich Asians","The Haunting","The Twin","Daughter of the Moon Godess",
+        "Daugter of the Moon Godess","Kept Secrets","The Outsiders","Pride and Prejudice","Beowulf"};
+
+
+        List<String> CheckedOutBooks = new ArrayList<>();
+        List<String> Books = new ArrayList<>(List.of(ArrayBooks)); 
 
         int exit = 0;
         Scanner input = new Scanner(System.in);
 
         while (exit < 1) {
-            
-            String[] Books = {
-                "The Clockwork Angel","Frankenstien","The Dare","Stalking Jack the Ripper",
-                "Crazy Rich Asians","The Haunting","The Twin","Daughter of the Moon Godess",
-                "Daugter of the Moon Godess","Kept Secrets","The Outsiders","Pride and Prejudice","Beowulf"};
 
-            display.Menu(Books);
+            display.Menu();
 
-            String userinput = input.nextLine();
+            String option = input.nextLine();
 
-            switch (userinput) {
-                case "6" -> exit = 1;
+            switch (option) {
+                
+                case "1" -> { 
+                System.out.println("What Book would you like to add? ");
+                String newbook = input.nextLine();
+                Books.add(newbook);
+                }
+                case "2" ->{ 
+                    System.out.println("What Book would you like to remove? ");
+                    String removebook = input.nextLine();
+                    Books.remove(removebook);
+                }
+                case "3" ->{
+                    System.out.println("What Book would you like to search for? ");
+                    String search = input.nextLine();
+
+                    if (Books.contains(search)) {
+                        System.out.println("We have that book");
+                    }
+
+                    else if (CheckedOutBooks.contains(search))
+                    {
+                        System.out.println("That book is checked out");
+                    }
+
+                    else
+                    {
+                        System.out.println("That Book is not in our System");
+                    }
+                    
+                }
+                case "4" -> display.Books(Books);
+
+                case "5" -> {
+                    System.out.println("What Book would you like to check out? ");
+                    String checkedBook = input.nextLine();
+                    if(Books.contains(checkedBook))
+                    {
+                        Books.remove(checkedBook);
+                        CheckedOutBooks.add(checkedBook);
+                    }
+                    else
+                    {
+                        System.out.println("We dont Have that Book");
+                    }
+                }
+                
+                case "6" ->
+                {
+                    System.out.println("What Book would you like to return? ");
+                    String returnedBook = input.nextLine();
+                    if(Books.contains(returnedBook))
+                    {
+                        CheckedOutBooks.remove(returnedBook);
+                        Books.add(returnedBook);
+                    }
+                    else
+                    {
+                        System.out.println("That book is not Checked out");
+                    }
+                }
+                
+                case "7" -> exit = 1;
             
                 default -> {
                 }
